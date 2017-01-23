ee00d13ff9cb24600f665fbb392a19183c218fbd
Scapain <dofustouch@no-emu.com>
2017-01-23 15:47:57 +0100

    autoload appVersion and buildVersion from apple API and proxy ankama

--- src/electron/application.ts
+++ src/electron/application.ts
@@ -1,43 +1,107 @@
 const settings = require('electron-settings');
-const { app, ipcMain } = require('electron');
+import {app, ipcMain} from 'electron';
+import * as request from 'request';
 
-import { DefaultSettings } from './default.settings';
-import { GameWindow } from './game-window';
-import { UpdateWindow } from './update-window';
+import {DefaultSettings} from './default.settings';
+import {GameWindow} from './game-window';
+import {UpdateWindow} from './update-window';
 
 export class Application {
 
     public website: string = "http://dofustouch.no-emu.com";
+    public cmdOptions: any;
     public devMode: boolean = false;
     private gameWindows: GameWindow[] = [];
     private updateWindow: UpdateWindow;
 
 
-    constructor() {
+    constructor(cmdOptions: any) {
         settings.defaults(DefaultSettings);
         settings.resetToDefaultsSync(); // debug
+        this.cmdOptions = cmdOptions;
         this.devMode = true/*settings.getSync('option.general.developer-mode')*/;
         this.updateWindow = new UpdateWindow(this);
     }
 
-    run(): void {
-        //this.updateWindow.run();
-        this.addWindow();
+    private getAppVersion(): Promise<string> {
+        return new Promise((resolve, reject) => {
+            request('https://itunes.apple.com/lookup?id=1041406978', function (error, response, body) {
 
-        ipcMain.on('load-config', (event, arg) => {
-            event.returnValue = {
-                gamePath : app.getPath('userData') + '/game'
-            }
-        })
+                if (!error && response.statusCode == 200) {
+                    let bodyParse = JSON.parse(body);
+                    resolve(bodyParse.results[0].version);
+                } else {
+                    reject(error);
+                }
+            });
+        });
+    }
+
+    private getBuildVersion(): Promise<string> {
+        return new Promise((resolve, reject) => {
+
+            request('https://proxyconnection.touch.dofus.com/build/script.js', (error, response, body) => {
+
+                if (!error && response.statusCode == 200) {
+                    const regex = /.*buildVersion=("|')([0-9]*\.[0-9]*\.[0-9]*)("|')/g;
+                    let m: RegExpExecArray;
+
+                    m = regex.exec(body.substring(1, 10000));
+                    resolve(m[2]);
+                } else {
+                    reject(error);
+                }
+            });
+        });
+    }
+
+
+    run(): void {
+        // get dynamic app and build version (avoid login block)
+
+        Promise.all([
+            this.getAppVersion(),
+            this.getBuildVersion(),
+        ]).then(([newAppVersion, newBuildVersion]) => {
+            settings.setSync('option.appVersion', newAppVersion);
+
+            //this.updateWindow.run().then(() => {
+            //this.addWindow();
+            //  this.updateWindow.win.close();
+            //});
+
+            /*if(this.cmdOptions.changelog){
+
+            }*/
+            this.addWindow();
+
+            ipcMain.on('load-config', (event, arg) => {
+
+                let appPath =  app.getAppPath();
+
+                if(this.cmdOptions.devmode){
+                    appPath = __dirname+'/../../';
+                }
+
+                event.returnValue = {
+                    gamePath: app.getPath('userData') + '/game',
+                    appPath: appPath,
+                    buildVersion: newBuildVersion,
+                    appVersion: newAppVersion
+                }
+            });
+        });
+
     }
 
     reloadSettings(): void {
-        // re bind shortcuts per game window
+
         this.gameWindows.forEach((gWindow) => {
-            gWindow.shortCuts.reload();
+            //gWindow.shortCuts.reload();
+            gWindow.reloadSettings();
         });
 
-        // reload main menu
+        // TODO: reload main menu
     }
 
     addWindow(): void {
